b533b0849bd725d8e02cff824a511359d825872a
aastha2517 <aastha2517@gmail.com>
2026-06-04 15:43:16 +0530

    Project-1 Time Converter

---
+++ Pr-1-Time-Converter/time-converter.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class time_convo{
+	public:
+		void sec_convo(int sec){
+			int min,h;
+			h=sec/3600;
+			cout<<"The hour of "<<sec<<" is :"<< h<<endl;
+			min=sec/60;
+			cout<<"The minutes of "<<sec<<" is :"<< min<<endl;
+		}
+		
+		void sec_count(int sec,int min,int h){
+			int ans;
+			ans=sec+(h*3600)+(min*60);
+			cout<<"The total second is :"<<ans<<endl;
+		}	
+};
+
+int main(){
+	
+	time_convo t1;
+	int choice,sec,min,h;
+	cout<<"Press 1 to convert second into Minutes and hours"<<endl;
+	cout<<"Press 2 to Count total second"<<endl;
+	cout<<"Enter Your Choice :";
+	cin>>choice;
+	
+	switch(choice){
+		case 1:
+			cout<<"Enter seconds to convert :";
+			cin>>sec;
+			t1.sec_convo(sec);
+			break;
+		
+		case 2:
+			cout<<"Enter seconds to count :";
+			cin>>sec;	
+			cout<<"Enter minutes to count :";
+			cin>>min;	
+			cout<<"Enter hours to count :";
+			cin>>h;	
+			t1.sec_count(sec,min,h);
+			break;		
+	}
+	return 0;
+}

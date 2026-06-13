bc056cf304386cd8f00290ec670adf58ffdc6721
Aastha_Patel <aastha2517@gmail.com>
2026-06-13 11:22:19 +0530

    Project 2 Railway Reservation System

--- pr-2-railway-reservation-system/railway-reservation.cpp
+++ pr-2-railway-reservation-system/railway-reservation.cpp
@@ -76,14 +76,14 @@ class RailwaySystem{
 			cout<<"Train Create Successfully......"<<endl;
 		}
 		
-		// Display Train Records Function
+		// 2.Display Train Records Function
 		void displayAllTrains(){
 			for(int i=0;i<totaltrains;i++){
 				trains[i].displayTrainDetail();
 			}
 		}
 		
-		// Search Train by Number Function
+		// 3.Search Train by Number Function
 		void searchTrainByNumber(int number){
 		    for(int i=0;i<totaltrains;i++){
 		        if(trains[i].trainNumber==number){
